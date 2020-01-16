e6b75041e57416e9b4dc91bedb88446da14fafe9
RConn12 <40410251+RConn12@users.noreply.github.com>
2020-01-16 11:15:47 -0500

    Initial commit

---
+++ app/src/main/java/eecs441/connrobe/chatter/MainActivity.java
@@ -0,0 +1,14 @@
+package eecs441.connrobe.chatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
