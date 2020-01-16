e0cab83e83c6c97b0145b1d9328a392d5e335c09
RConn12 <40410251+RConn12@users.noreply.github.com>
2020-01-16 11:37:05 -0500

    Example app up to the Chatt list.

---
+++ app/src/main/java/eecs441/connrobe/chatter/PostActivity.java
@@ -0,0 +1,14 @@
+package eecs441.connrobe.chatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
