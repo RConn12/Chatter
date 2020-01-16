037857955de3da63c3722ff0f2f6e2392166297b
RConn12 <40410251+RConn12@users.noreply.github.com>
2020-01-16 14:09:43 -0500

    Example app up to the Chatt list.

---
+++ app/src/main/java/eecs441/connrobe/chatter/Chatt.java
@@ -0,0 +1,12 @@
+package eecs441.connrobe.chatter;
+
+public class Chatt {
+    public String username;
+    public String message;
+    public String timestamp;
+    public Chatt(String username, String message, String timestamp) {
+        this.username = username;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
